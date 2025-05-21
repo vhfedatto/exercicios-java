1565afabd4d5ae08e4774bb0bc44477e1e12b68f
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-05-21 11:43:27 -0300

    Matrizes
    Adição do primeiro código do repositório. O código padrão para criação de matrizes.

---
+++ matrizes.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class matrizes{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int matriz [] [] = new int[3][3];
+        
+        for(int i = 0; i<matriz.length; i++){
+            for(int j = 0; j<matriz[i].length; j++){
+                System.out.print("Digite o valor da linha ["+i+"] coluna ["+j+"]: ");
+                int num = input.nextInt();
+                matriz[i][j] = num;
+            }
+        }
+
+        System.out.println("Matriz Digitada:");
+        for(int i = 0; i < matriz.length; i++){
+            for (int j = 0; j < matriz[i].length; j++){
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+        input.close();
+
+    }
+}
