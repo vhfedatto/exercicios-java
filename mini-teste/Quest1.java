9d708c41392795f45e9aad6badfd951305c4f86d
Victor Hugo Fedatto <victorhugofedatto@gmail.com>
2025-05-21 16:40:15 -0300

    Adição de questões novas

---
+++ mini-teste/Quest1.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Quest1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double Notas[] = new double [10];
+        
+        double soma = 0;
+        int c = 0;
+        int acima = 0;
+        
+        for(int i = 0; i<10; i++){
+            
+            System.out.print("Aluno "+(i+1)+": Qual a nota? ");
+            double nota = input.nextDouble();
+            c++;
+            Notas[i] = nota;
+            
+            soma += nota;
+        }
+        
+        double media = 0;
+        media = soma / c;
+        
+        System.out.println("\nMédia das notas: "+ String.format("%.2f", media));
+        System.out.println("=== Notas acima da média ===");
+        for(double nota : Notas){
+
+            //Estou considerando que a média é 7;
+            if(nota >= 7){
+                System.out.println(nota);
+                acima++;
+            }
+        }
+        if(acima == 0){
+            System.out.println("Não há notas acima da média");   
+        }
+        System.out.println("Quantidade de notas abaixo da média: "+(10-acima));
+        input.close();
+    }
+}
---
+++ mini-teste/Quest2.java
@@ -0,0 +1,111 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Quest2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int CodProd [] = new int[8];
+        String x = "N";
+        
+        while(x.equalsIgnoreCase("N")){
+            int z = 0;
+            System.out.println("\n=== Sistema Gerenciador de Códigos ===");
+            System.out.println("1. Adicionar códigos\n2. Buscar Código\n3. Exibir lista\n4. Sair");
+            System.out.print("== Comando: ");
+            int dec = input.nextInt();    
+            
+            if(dec == 1){
+                if(CodProd.length == 8){
+                    for(int el : CodProd){
+                        if(el == 0){
+                            z++;
+                        }
+                    }
+                    if(z == 8){
+                        System.out.println(">> Por padrão, adicionarás 8 códigos <<");
+                        for(int i = 0; i < CodProd.length; i++){
+                            boolean valido = false;
+                            
+                            while(!valido){
+                                System.out.print("Digite o código do produto " + (i + 1) + ": ");
+                                int cod = input.nextInt();
+                                input.nextLine(); // Limpa buffer
+
+                                boolean jaExiste = false;
+
+                                // Verifica se o código já existe no vetor
+                                for(int j = 0; j < i; j++){
+                                    if(CodProd[j] == cod){
+                                        jaExiste = true;
+                                        break;
+                                    }
+                                }
+
+                                if(jaExiste){
+                                    System.out.println(">> Código: " + cod + " já existe no vetor. Digite outro. <<");
+                                } else {
+                                    CodProd[i] = cod;
+                                    valido = true; // Sai do while
+                                }
+                            }
+                        }
+
+                    }else{
+                        System.out.println("= O vetor já está cheio =");
+                    }
+                }
+            //Buscar código
+            }else if(dec == 2){
+                System.out.print("Buscar Código: ");
+                int pesquisa = input.nextInt();
+
+                boolean encontrado = false;
+
+                for(int j = 0; j < CodProd.length; j++){
+                    int codigoProdPesquisa = CodProd[j];
+
+                    if(pesquisa == codigoProdPesquisa){
+                        System.out.println(">> Código: "+pesquisa+" já existe no vetor <<");
+                        encontrado = true;
+                    }
+                }
+
+                if(!encontrado){
+                    System.out.println(">> Código não encontrado <<");
+                }
+
+            //Exibir lista completa
+            }else if(dec == 3){
+                System.out.println("\n=== Apresentando a Lista de Códigos ===");
+                Arrays.sort(CodProd);
+                for(int codigo : CodProd){
+                    System.out.print(codigo+" ");
+                }
+                System.out.println(" ");
+                System.out.println("===============================");
+
+            //Sair do programa
+            }else if(dec == 4){
+                input.nextLine(); 
+                System.out.print("\nDeseja mesmo sair? [S/N] ");
+                x = input.nextLine();
+                System.out.println("===== Saindo do programa =====");
+
+            }else{
+                System.out.println("Opção inválida");
+            }
+
+        }
+
+        //Imprimindo a lista ao sair
+        System.out.println("\n=== Apresentando a Lista de Códigos ===");
+        Arrays.sort(CodProd);
+        for(int codigo : CodProd){
+            System.out.print(codigo+" ");
+        }
+        System.out.println(" ");
+        System.out.println("===============================");
+        input.close();
+    }
+}
